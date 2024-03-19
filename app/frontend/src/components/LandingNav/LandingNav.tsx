19189f8897733f67be3eec4c519002f0c5c05826
tris <tris.bouden@gmail.com>
2024-03-19 16:58:00 +0100

    create simple header for the Landing page

--- app/frontend/src/components/LandingNav/LandingNav.tsx
+++ app/frontend/src/components/LandingNav/LandingNav.tsx
@@ -20,46 +20,47 @@ export default function LandingNav({ currentPage }: LandingNavProps) {
   const { text, link } = getButtonTextAndLink();
 
   return (
-    <nav className="bg-white sm:px-8 px-3 py-3 flex items-center justify-between">
-      {/* Logo */}
-      <Link to="/" className="flex items-end">
-        <span className="text-rose-600 font-extrabold text-4xl">Z</span>
-        <span className="text-gray-800 font-semibold text-3xl">enith</span>
-      </Link>
-
-      {/* Navigation Links */}
-      <div className="flex items-center space-x-5">
-        <Link
-          to="/"
-          className="hidden sm:flex text-gray-500 hover:text-gray-900"
-        >
-          Home
+    <nav className="bg-white">
+      <div className="container m-auto px-3 py-3 flex items-center justify-between">
+        {/* Logo */}
+        <Link to="/" className="flex items-end">
+          <span className="text-rose-600 font-extrabold text-4xl">Z</span>
+          <span className="text-gray-800 font-semibold text-3xl">enith</span>
         </Link>
-        <Link
-          to="/#features"
-          className="hidden sm:flex text-gray-500 hover:text-gray-900"
-        >
-          Features
-        </Link>
-        <Link
-          to="/#about"
-          className="hidden sm:flex text-gray-500 hover:text-gray-900"
-        >
-          About
-        </Link>
-        <Link
-          to="/#help"
-          className="hidden sm:flex text-gray-500 hover:text-gray-900"
-        >
-          Help
-        </Link>
-        {/* Dynamic Button */}
-        <Link
-          to={link}
-          className="bg-rose-500 text-white font-semibold px-4 py-[5px] rounded-xl transition duration-300 shadow-md transform hover:scale-105 hover:bg-rose-600"
-        >
-          {text}
-        </Link>
+        {/* Navigation Links */}
+        <div className="flex items-center space-x-5">
+          <Link
+            to="/"
+            className="hidden sm:flex text-gray-500 hover:text-gray-900"
+          >
+            Home
+          </Link>
+          <Link
+            to="/#features"
+            className="hidden sm:flex text-gray-500 hover:text-gray-900"
+          >
+            Features
+          </Link>
+          <Link
+            to="/#about"
+            className="hidden sm:flex text-gray-500 hover:text-gray-900"
+          >
+            About
+          </Link>
+          <Link
+            to="/#help"
+            className="hidden sm:flex text-gray-500 hover:text-gray-900"
+          >
+            Help
+          </Link>
+          {/* Dynamic Button */}
+          <Link
+            to={link}
+            className="bg-rose-500 text-white font-semibold px-4 py-[5px] rounded-xl transition duration-300 shadow-md transform hover:scale-105 hover:bg-rose-600"
+          >
+            {text}
+          </Link>
+        </div>
       </div>
     </nav>
   );
--- app/frontend/src/pages/Landing/Landing.tsx
+++ app/frontend/src/pages/Landing/Landing.tsx
@@ -1,11 +1,36 @@
 import LandingNav from "../../components/LandingNav/LandingNav";
-
+import "./Landing.scss";
+import headerMap from "./../../assets/header-map.png";
+import { Link } from "react-router-dom";
 export default function Landing() {
   return (
     <>
-      <div>
-        <LandingNav />
-        <h1>Hello</h1>
+      <LandingNav />
+      <div className="landing container m-auto px-3">
+        <header className="w-full h-screen">
+          <div className="intro md:py-3 md:rounded-xl space-y-10 lg:w-3/5 md:w-[520px] bg-opacity-80 bg-white">
+            <h1 className="text-gray-900 text-5xl font-bold">
+              Climb To New Heights With{" "}
+              <span className="text-rose-600 font-bold">Zenith</span>
+            </h1>
+            <p className="text-gray-500 text-lg">
+              Your Ultimate Companion for Peak Exploration! Whether you're an
+              avid adventurer, nature enthusiast, or simply seeking breathtaking
+              views, Zenith helps you discover and conquer the highest points
+              wherever you roam.
+            </p>
+            <button className="text-rose-600 px-3 py-2 border-rose-600 rounded-xl border-2 btn-tomap">
+              <Link to="/map">Start Now!</Link>
+            </button>
+          </div>
+          <div className="lg:w-2/5 hidden md:block md:absolute lg:static md:right-0 md:py-8 lg:p-0 md:z-[-1] lg:z-0 w-2/4  h-full rounded-2xl">
+            <img
+              className="h-full object-cover rounded-2xl"
+              src={headerMap}
+              alt="Google Map Image"
+            />
+          </div>
+        </header>
       </div>
     </>
   );
