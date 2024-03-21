5bbf453d956aaed576cef9934779f3b5520b9659
Arky Asmal <arkyasmal@gmail.com>
2024-03-21 04:03:52 -0400

    added prisma initialization, and connection when running startServer script

--- app/backend/scripts/startServer.py
+++ app/backend/scripts/startServer.py
@@ -1,5 +1,24 @@
 import subprocess
-# Insert the command you want to execute in the command line
-command = "prisma"
-
-subprocess.call(command, shell=True)
+import sys
+from dotenv import load_dotenv
+from manage import main as start_django_server
+def grab_secrets(): 
+    command = "doppler secrets download --no-file --format env > .env"
+    subprocess.call(command, shell=True)
+def generate_prisma_schema():
+    """
+    Generates a Prisma schema from the Prisma API.
+    """
+    # Define the command to execute in the command line
+    # command = "python -m prisma db push --schema '../../prisma/schema.prisma'"
+    subprocess.call([sys.executable, '-m', 'prisma', 'db', 'push', '--schema', './prisma/schema.prisma'])
+def main():
+    #initialize secrets
+    grab_secrets()
+    load_dotenv()
+    #generate the prisma schema
+    generate_prisma_schema()
+    #start django server
+    start_django_server()
+if __name__ == '__main__':
+    main()
