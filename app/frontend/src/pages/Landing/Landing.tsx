ef2cda34d972e511eef631591231a4c442042576
tris <tris.bouden@gmail.com>
2024-03-21 09:59:37 +0100

    Edit feature section parallax scrolling animation and Add hover effect to Get started button

--- app/frontend/src/pages/Landing/Landing.tsx
+++ app/frontend/src/pages/Landing/Landing.tsx
@@ -27,10 +27,10 @@ export default function Landing() {
               wherever you roam.
             </p>
             <button className="text-rose-600 px-3 py-2 border-rose-600 rounded-xl border-2 btn-tomap">
-              <Link to="/map">Start Now!</Link>
+              <Link to="/map">Get Started!</Link>
             </button>
             <div className="relative w-full h-1">
-              <ParallaxLayer offset={0.1} speed={0.5}>
+              <ParallaxLayer offset={0.1} speed={0.5} className="z-10">
                 <div className="rounded-2xl ">
                   <img
                     className="w-full h-96 object-cover rounded-2xl m-auto"
@@ -43,50 +43,42 @@ export default function Landing() {
           </div>
 
           {/* Features */}
-          <div id="features">
+          <div id="features" className="mt-60">
             {/* feature 1 */}
-            <ParallaxLayer
-              offset={1.3}
-              speed={0.7}
-              className="flex justify-end w-full max-w-96 p-3 md:p-5 ml-auto"
-            >
-              <img
-                className="w-full object-cover h-[500px]"
-                src={feat1}
-                alt="Discover Elevation Anywhere"
-              />
-            </ParallaxLayer>
-            <ParallaxLayer offset={1.1}>
-              <div className="flex justify-start w-full max-w-[500px] flex-col p-3 md:p-5 space-y-5 bg-white bg-opacity-80 rounded-2xl">
-                <h2 className="text-rose-600 text-3xl font-bold ">
+            <div className=" my-20 flex flex-wrap sm:flex-nowrap">
+              <div className="items-center flex justify-start w-full max-w-[500px] sm:w-3/5 flex-col p-3 md:p-5 space-y-5 bg-white">
+                <h2 className="text-rose-600 text-4xl font-bold ">
                   Discover Elevation Anywhere
                 </h2>
-                <p>
+                <p className="text-gray-600 text-lg leading-7">
                   Click, select, and uncover the highest elevation in any area
                   with Zenith. Whether you're planning your next hike or
                   exploring new terrain, effortlessly access elevation data to
                   make informed decisions and conquer new heights.
                 </p>
               </div>
-            </ParallaxLayer>
+
+              <div className="relative h-[400px] flex-1 min-w-64">
+                <ParallaxLayer
+                  offset={0.15}
+                  speed={0.1}
+                  className="flex justify-end w-full p-3 md:p-5"
+                >
+                  <img
+                    className="object-cover rounded-lg h-[300px] min-w-[80%]"
+                    src={feat1}
+                    alt="Discover Elevation Anywhere"
+                  />
+                </ParallaxLayer>
+              </div>
+            </div>
             {/* feature 2 */}
-            <ParallaxLayer
-              offset={2}
-              speed={0.4}
-              className="flex justify-end w-full max-w-96 p-3 md:p-5 ml-auto"
-            >
-              <img
-                className="w-full object-cover h-[400px]"
-                src={feat2}
-                alt="Plan Your Adventures with Precision"
-              />
-            </ParallaxLayer>
-            <ParallaxLayer offset={2.1}>
-              <div className="flex justify-start w-full max-w-[500px] flex-col p-3 md:p-5 space-y-5 bg-white bg-opacity-80 rounded-2xl">
-                <h2 className="text-rose-600 text-3xl font-bold ">
+            <div className=" my-20 flex flex-wrap sm:flex-nowrap">
+              <div className="flex items-center justify-start w-full max-w-[500px] sm:w-3/5 flex-col p-3 md:p-5 space-y-5 bg-white">
+                <h2 className="text-rose-600 text-4xl font-bold ">
                   Plan Your Adventures with Precision
                 </h2>
-                <p>
+                <p className="text-gray-600 text-lg leading-7">
                   With Zenith, precision planning is at your fingertips.
                   Pinpoint the highest peaks, assess terrain difficulty, and
                   chart your course with confidence. Empower your outdoor
@@ -94,25 +86,28 @@ export default function Landing() {
                   navigation.
                 </p>
               </div>
-            </ParallaxLayer>
+
+              <div className="relative h-[400px] flex-1 min-w-64">
+                <ParallaxLayer
+                  offset={0.19}
+                  speed={0.1}
+                  className="flex justify-end w-full p-3 md:p-5 mt-5 sm:mt-0"
+                >
+                  <img
+                    className="object-cover rounded-lg h-[400px] lg:h-[50%] min-w-[80%]"
+                    src={feat2}
+                    alt="Plan Your Adventures with Precision"
+                  />
+                </ParallaxLayer>
+              </div>
+            </div>
             {/* feature 3 */}
-            <ParallaxLayer
-              offset={2.9}
-              speed={0.5}
-              className="flex justify-end w-full max-w-96 p-3 md:p-5 ml-auto"
-            >
-              <img
-                className="w-full object-cover h-[500px]"
-                src={feat3}
-                alt="Community Exploration"
-              />
-            </ParallaxLayer>
-            <ParallaxLayer offset={2.9}>
-              <div className="flex justify-start w-full max-w-[500px] flex-col p-3 md:p-5 space-y-5 bg-white bg-opacity-80 rounded-2xl">
-                <h2 className="text-rose-600 text-3xl font-bold ">
+            <div className=" my-20 flex flex-wrap sm:flex-nowrap">
+              <div className="flex items-center justify-start w-full max-w-[500px] sm:w-3/5 flex-col p-3 md:p-5 space-y-5 bg-white">
+                <h2 className="text-rose-600 text-4xl font-bold ">
                   Seamless Sharing, Endless Exploration
                 </h2>
-                <p>
+                <p className="text-gray-600 text-lg leading-7">
                   Share your discoveries and inspire others with Zenith's
                   seamless sharing features. Export shareable links that open
                   directly in popular mapping apps, allowing you to share your
@@ -120,7 +115,21 @@ export default function Landing() {
                   explorers and embark on endless adventures together.
                 </p>
               </div>
-            </ParallaxLayer>
+
+              <div className="relative h-[400px] flex-1 min-w-64">
+                <ParallaxLayer
+                  offset={0.34}
+                  speed={0.1}
+                  className="flex justify-end w-full p-3 md:p-5"
+                >
+                  <img
+                    className="object-cover rounded-lg h-[350px] min-w-[80%]"
+                    src={feat3}
+                    alt="Community Exploration"
+                  />
+                </ParallaxLayer>
+              </div>
+            </div>
           </div>
         </div>
       </Parallax>
